c855ae4a5223d0360b2348f36e24797c49d2e94d
ljbellido <ljbellido@106-14.info2>
2024-02-29 13:47:29 +0100

    Rama Lorenzo Primera Subida

--- src/minesweeper/Functions.java
+++ src/minesweeper/Functions.java
@@ -1,8 +1,25 @@
 package minesweeper;
 
 public class Functions {
-	public final int[] BOARDSIZE= {5,5};
-	public final int MINESNUMBER = 5;
+	public static final int M= 5;
+	public static final int N= 5;
+	public static char [][] board = {{'B','X','X','X','X'},{'X','X','X','X','X'},{'X','X','X','X','X'},{'X','X','X','X','X'},{'X','X','X','X','X'}};
+	public static char [][] boardNumbers = new char[M][N];
+	public static final int MINESNUMBER = 5;
 	public int positionsRevealed = 0;
+	public static int posicionX = 0;
+	public static int posicionJ = 0;
+	
+	
+	public static void modifyBoard() {
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				if('B' == board[i][j]) {
+					
+				}
+			}
+		}
+	}
+	
 	
 }
