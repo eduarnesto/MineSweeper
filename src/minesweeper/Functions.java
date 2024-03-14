3d7ab029fe9d2f72e4cbcf080bb51b322456abc7
ljbellido <ljbellido@106-14.info2>
2024-03-14 13:48:38 +0100

    lorenzo mine

--- src/minesweeper/Functions.java
+++ src/minesweeper/Functions.java
@@ -1,25 +1,38 @@
 package minesweeper;
 
+import java.util.Random;
+
 public class Functions {
-	public static final int M= 5;
-	public static final int N= 5;
-	public static char [][] board = {{'B','X','X','X','X'},{'X','X','X','X','X'},{'X','X','X','X','X'},{'X','X','X','X','X'},{'X','X','X','X','X'}};
-	public static char [][] boardNumbers = new char[M][N];
+	public static final int I = 5;
+	public static final int J = 5;
+	public static char[][] board = { { 'X', 'B', 'X', 'B', 'X' }, { 'X', 'X', 'B', 'X', 'X' },
+			{ 'X', 'X', 'X', 'X', 'X' }, { 'X', 'X', 'X', 'X', 'X' }, { 'X', 'X', 'X', 'X', 'X' } };
+	public static int[][] boardNumbers = { { 0, -1, 0, -1, 0 }, { 0, 0, -1, 0, 0 },
+			{ 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
 	public static final int MINESNUMBER = 5;
 	public int positionsRevealed = 0;
-	public static int posicionX = 0;
-	public static int posicionJ = 0;
-	
-	
+	public static int positionX = 0;
+	public static int positionJ = 0;
+
 	public static void modifyBoard() {
+		int contador = 1;
 		for(int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board[i].length; j++) {
-				if('B' == board[i][j]) {
+			for(int j = 0; j < board.length; j++) {
+				if(boardNumbers[i][j] != -1) {
+					if(j > 0 && j < boardNumbers.length && boardNumbers[i][j - 1] == -1) {
+						boardNumbers[i][j]++;
+					}
+					if(j >= 0 && j < boardNumbers.length - 1 && boardNumbers[i][j + 1] == -1) {
+						boardNumbers[i][j]++;
+					}
 					
+				}else {
+					contador = 0;
 				}
+				
+				contador = 0;
 			}
 		}
 	}
-	
 	
 }
--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -1,11 +1,24 @@
 package minesweeper;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int playerPos;
-		boolean win = false;
+		for(char fila[] : Functions.board) {
+			for(char valor : fila) {
+				System.out.print(valor + " ");
+			}
+			System.out.println();
+		}
+		Functions.modifyBoard();
 		
+		for(int fila[] : Functions.boardNumbers) {
+			for(int valor : fila) {
+				System.out.print(valor + " ");
+			}
+			System.out.println();
+		}
 
 	}
 
