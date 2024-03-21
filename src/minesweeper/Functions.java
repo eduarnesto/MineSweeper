6b6cc434e58836873e87281f501e49168aad6cc8
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2024-03-21 12:02:31 +0100

    modifyBoard 1.1

--- src/minesweeper/Functions.java
+++ src/minesweeper/Functions.java
@@ -6,9 +6,9 @@ public class Functions {
 	public static final int I = 5;
 	public static final int J = 5;
 	public static char[][] board = { { 'X', 'B', 'X', 'B', 'X' }, { 'X', 'X', 'B', 'X', 'X' },
-			{ 'X', 'X', 'X', 'X', 'X' }, { 'X', 'X', 'X', 'X', 'X' }, { 'X', 'X', 'X', 'X', 'X' } };
+			{ 'X', 'X', 'X', 'X', 'X' }, { 'X', 'X', 'X', 'B', 'X' }, { 'X', 'X', 'X', 'X', 'X' } };
 	public static int[][] boardNumbers = { { 0, -1, 0, -1, 0 }, { 0, 0, -1, 0, 0 },
-			{ 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+			{ 0, 0, 0, 0, 0 }, { 0, 0, 0, -1, 0 }, { 0, 0, 0, 0, 0 } };
 	public static final int MINESNUMBER = 5;
 	public int positionsRevealed = 0;
 	public static int positionX = 0;
@@ -19,10 +19,16 @@ public class Functions {
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board.length; j++) {
 				if(boardNumbers[i][j] != -1) {
-					if(j > 0 && j < boardNumbers.length && boardNumbers[i][j - 1] == -1) {
+					if(j > 0 && j < boardNumbers[i].length && boardNumbers[i][j - 1] == -1) {
 						boardNumbers[i][j]++;
 					}
-					if(j >= 0 && j < boardNumbers.length - 1 && boardNumbers[i][j + 1] == -1) {
+					if(j >= 0 && j < boardNumbers[i].length - 1 && boardNumbers[i][j + 1] == -1) {
+						boardNumbers[i][j]++;
+					}
+					if(i > 0 && i < boardNumbers.length && boardNumbers[i - 1][j] == -1) {
+						boardNumbers[i][j]++;
+					}
+					if(i >= 0 && i < boardNumbers.length - 1 && boardNumbers[i + 1][j] == -1) {
 						boardNumbers[i][j]++;
 					}
 					
