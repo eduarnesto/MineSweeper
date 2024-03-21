feefcb6a1bdb13ba74b4992c97d03779581c09f6
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2024-03-21 12:03:24 +0100

    modifyBoard 1.2

--- src/minesweeper/Functions.java
+++ src/minesweeper/Functions.java
@@ -15,7 +15,6 @@ public class Functions {
 	public static int positionJ = 0;
 
 	public static void modifyBoard() {
-		int contador = 1;
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board.length; j++) {
 				if(boardNumbers[i][j] != -1) {
@@ -32,11 +31,9 @@ public class Functions {
 						boardNumbers[i][j]++;
 					}
 					
-				}else {
-					contador = 0;
 				}
 				
-				contador = 0;
+				
 			}
 		}
 	}
