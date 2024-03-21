cced0de878fe8b280a3efbbe67e357faa9690ed9
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2024-03-21 12:11:16 +0100

    modifyBoard 1.3

--- src/minesweeper/Functions.java
+++ src/minesweeper/Functions.java
@@ -7,35 +7,34 @@ public class Functions {
 	public static final int J = 5;
 	public static char[][] board = { { 'X', 'B', 'X', 'B', 'X' }, { 'X', 'X', 'B', 'X', 'X' },
 			{ 'X', 'X', 'X', 'X', 'X' }, { 'X', 'X', 'X', 'B', 'X' }, { 'X', 'X', 'X', 'X', 'X' } };
-	public static int[][] boardNumbers = { { 0, -1, 0, -1, 0 }, { 0, 0, -1, 0, 0 },
-			{ 0, 0, 0, 0, 0 }, { 0, 0, 0, -1, 0 }, { 0, 0, 0, 0, 0 } };
+	public static int[][] boardNumbers = { { 0, -1, 0, -1, 0 }, { 0, 0, -1, 0, 0 }, { 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, -1, 0 }, { 0, 0, 0, 0, 0 } };
 	public static final int MINESNUMBER = 5;
 	public int positionsRevealed = 0;
 	public static int positionX = 0;
 	public static int positionJ = 0;
 
 	public static void modifyBoard() {
-		for(int i = 0; i < boardNumbers.length; i++) {
-			for(int j = 0; j < boardNumbers[i].length; j++) {
-				if(boardNumbers[i][j] != -1) {
-					if(j > 0 && j < boardNumbers[i].length && boardNumbers[i][j - 1] == -1) {
+		for (int i = 0; i < boardNumbers.length; i++) {
+			for (int j = 0; j < boardNumbers[i].length; j++) {
+				if (boardNumbers[i][j] != -1) {
+					if (j > 0 && j < boardNumbers[i].length && boardNumbers[i][j - 1] == -1) {
 						boardNumbers[i][j]++;
 					}
-					if(j >= 0 && j < boardNumbers[i].length - 1 && boardNumbers[i][j + 1] == -1) {
+					if (j >= 0 && j < boardNumbers[i].length - 1 && boardNumbers[i][j + 1] == -1) {
 						boardNumbers[i][j]++;
 					}
-					if(i > 0 && i < boardNumbers.length && boardNumbers[i - 1][j] == -1) {
+					if (i > 0 && i < boardNumbers.length && boardNumbers[i - 1][j] == -1) {
 						boardNumbers[i][j]++;
 					}
-					if(i >= 0 && i < boardNumbers.length - 1 && boardNumbers[i + 1][j] == -1) {
+					if (i >= 0 && i < boardNumbers.length - 1 && boardNumbers[i + 1][j] == -1) {
 						boardNumbers[i][j]++;
 					}
-					
+
 				}
-				
-				
+
 			}
 		}
 	}
-	
+
 }
--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -5,21 +5,6 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		for(char fila[] : Functions.board) {
-			for(char valor : fila) {
-				System.out.print(valor + "");
-			}
-			System.out.println();
-		}
-		Functions.modifyBoard();
-		System.out.println();
-		for(int fila[] : Functions.boardNumbers) {
-			for(int valor : fila) {
-				System.out.print(valor + "\t");
-			}
-			System.out.println();
-		}
 
 	}
-
 }
