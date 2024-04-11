b395a0a8d0a387968233583a7ca9c1efd0d40050
Marco55H <145675712+Marco55H@users.noreply.github.com>
2024-04-11 14:37:00 +0300

    Marco

--- src/minesweeper/Functions.java
+++ src/minesweeper/Functions.java
@@ -1,8 +1,51 @@
 package minesweeper;
 
+import java.util.Random;
+
 public class Functions {
-	public final int[] BOARDSIZE= {5,5};
-	public final int MINESNUMBER = 5;
-	public int positionsRevealed = 0;
+	public static final int I = 5;
+	public static final int J = 5;
+	public static char[][] board = new char[I][J];
+	public static int[][] boardNumbers = new int[I][J];
+	public static final int MINESNUMBER = 5;
+	public int revealedPositions = 0;
+
 	
+	public static void createBoard() {
+ 
+		for (int i = 0; i < board.length; i++) {
+			System.out.print("  " + i);
+		}
+		System.out.println();
+		for (int i = 0; i < board.length; i++) {
+			System.out.print((i) + " ");
+			for (int j = 0; j < board[0].length; j++) {
+				board[i][j] = 'x';
+				System.out.print(board[i][j] + "  ");
+			}
+			System.out.println();
+		}
+	}
+
+	
+	public static void placeMines() {
+		Random r = new Random();
+		int mineI;
+		int mineJ;
+
+		for (int i = 0; i < board.length; i++) {
+			int cont = 0;
+
+			do {
+				mineI = r.nextInt(0, I);
+				mineJ = r.nextInt(0, J);
+			} while (boardNumbers[mineI][mineJ] == -1);
+
+			boardNumbers[mineI][mineJ] = -1;
+
+			System.out.println(mineI + " " + mineJ);
+			System.out.println(boardNumbers[mineI][mineJ]);
+
+		}
+	}
 }
--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -6,7 +6,16 @@ public class Main {
 		int playerPos;
 		boolean win = false;
 		
+		Functions.createBoard();
 
+		Functions.placeMines();
+		
+		for(int[] numeron : Functions.boardNumbers) {
+			for(int num : numeron) {
+				System.out.print(num + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 }
