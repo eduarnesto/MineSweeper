5dfed40e461748956d7a6299cfdcc67d19ef8922
LorenzoBellido <lorenzo.bellido@iesnervion.es>
2024-04-11 13:43:05 +0200

    Main

--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -1,25 +1,10 @@
 package minesweeper;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int playerPos;
-		boolean win = false;
-<<<<<<< HEAD
-		Functions.showBoard();
-=======
-		
-		Functions.createBoard();
->>>>>>> refs/heads/Marco
 
-		Functions.placeMines();
-		
-		for(int[] numeron : Functions.boardNumbers) {
-			for(int num : numeron) {
-				System.out.print(num + "\t");
-			}
-			System.out.println();
-		}
 	}
-
 }
